2ae68b4562b99a93ff69b10cd2bbd48117025351
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2018-03-05 12:55:45 -0500

    redone template-syntax

--- fundamentals/02-template/template-syntax/e2e/app.e2e-spec.ts
+++ fundamentals/02-template/template-syntax/e2e/app.e2e-spec.ts
@@ -1,43 +1,14 @@
-'use strict'; // necessary for es6 output in node
+import { AppPage } from './app.po';
 
-import { browser, element, by } from 'protractor';
+describe('template-syntax App', () => {
+  let page: AppPage;
 
-// Not yet complete
-describe('Template Syntax', function () {
-
-  beforeAll(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new AppPage();
   });
 
-  it('should be able to use interpolation with a hero', function () {
-    let heroInterEle = element.all(by.css('h2+p')).get(0);
-    expect(heroInterEle.getText()).toEqual('My current hero is Hercules');
-  });
-
-  it('should be able to use interpolation with a calculation', function () {
-    let theSumEles = element.all(by.cssContainingText('h3~p', 'The sum of'));
-    expect(theSumEles.count()).toBe(2);
-    expect(theSumEles.get(0).getText()).toEqual('The sum of 1 + 1 is 2');
-    expect(theSumEles.get(1).getText()).toEqual('The sum of 1 + 1 is not 4');
-  });
-
-  it('should be able to use class binding syntax', function () {
-    let specialEle = element(by.cssContainingText('div', 'Special'));
-    expect(specialEle.getAttribute('class')).toMatch('special');
-  });
-
-  it('should be able to use style binding syntax', function () {
-    let specialButtonEle = element(by.cssContainingText('div.special~button', 'button'));
-    expect(specialButtonEle.getAttribute('style')).toMatch('color: red');
-  });
-
-  it('should two-way bind to sizer', async () => {
-    let div = element(by.css('div#two-way-1'));
-    let incButton = div.element(by.buttonText('+'));
-    let input = div.element(by.css('input'));
-    let initSize = await input.getAttribute('value');
-    incButton.click();
-    expect(input.getAttribute('value')).toEqual((+initSize + 1).toString());
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
-
