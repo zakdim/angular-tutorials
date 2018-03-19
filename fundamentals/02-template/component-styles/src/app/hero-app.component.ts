97ec33b049d173cbab9e1e250556b15c0b7c16b5
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2018-03-19 00:16:31 -0400

    component-styles init

---
+++ fundamentals/02-template/component-styles/src/app/hero-app.component.ts
@@ -0,0 +1,31 @@
+import { Component, HostBinding } from '@angular/core';
+import { Hero } from './hero';
+
+@Component({
+  selector: 'app-root',
+  // template: `
+  //   <h1>Tour of Heroes</h1>
+  //   <app-hero-main [hero]="hero"></app-hero-main>
+  // `,
+  template: `
+  <h1>Tour of Heroes</h1>
+`,
+  styles: ['h1 { font-weight: normal; }']
+})
+export class HeroAppComponent {
+  hero = new Hero(
+    'Human Torch',
+    ['Mister Fantastic', 'Invisible Woman', 'Thing']
+  );
+
+  @HostBinding('class') get themeClass() {
+    return 'theme-light';
+  }
+}
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
