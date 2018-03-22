d12320b9d736bcc0a712fcec7ad9932c3b334dd5
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2018-03-21 22:58:51 -0400

    pipes examples

--- fundamentals/02-components-templates/pipes/src/app/app.module.ts
+++ fundamentals/02-components-templates/pipes/src/app/app.module.ts
@@ -1,18 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+// import {
+//   FlyingHeroesComponent,
+//   FlyingHeroesImpureComponent
+// } from './flying-heroes.component';
+// import { HeroAsyncMessageComponent } from './hero-async-message.component';
+import { HeroBirthdayComponent } from './hero-birthday1.component';
+import { HeroBirthday2Component } from './hero-birthday2.component';
+// import { HeroListComponent } from './hero-list.component';
+// import { PowerBoosterComponent } from './power-booster.component';
+// import { PowerBoostCalculatorComponent } from './power-boost-calculator.component';
+// import {
+//   FlyingHeroesPipe,
+//   FlyingHeroesImpurePipe
+// } from './flying-heroes.pipe';
+// import { FetchJsonPipe } from './fetch-json.pipe';
+// import { ExponentialStrengthPipe } from './exponential-strength.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    // FlyingHeroesComponent,
+    // FlyingHeroesImpureComponent,
+    // HeroAsyncMessageComponent,
+    HeroBirthdayComponent,
+    HeroBirthday2Component,
+    // HeroListComponent,
+    // PowerBoosterComponent,
+    // PowerBoostCalculatorComponent,
+    // FlyingHeroesPipe,
+    // FlyingHeroesImpurePipe,
+    // FetchJsonPipe,
+    // ExponentialStrengthPipe
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- fundamentals/02-components-templates/pipes/src/app/hero-birthday1.component.ts
+++ fundamentals/02-components-templates/pipes/src/app/hero-birthday1.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: 'app-hero-birthday',
+  template: `<p>The hero's birthday is {{ birthday | date }}</p>`
 })
-export class AppComponent {
+export class HeroBirthdayComponent {
   birthday = new Date(1988, 3, 15); // April 15, 1988
 }
 
