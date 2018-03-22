61af57189a2c1a82341185bb95049b85fb1058d3
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2018-03-21 23:43:42 -0400

    pipes

--- fundamentals/02-components-templates/pipes/src/app/app.module.ts
+++ fundamentals/02-components-templates/pipes/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-// import {
-//   FlyingHeroesComponent,
-//   FlyingHeroesImpureComponent
-// } from './flying-heroes.component';
+import {
+  FlyingHeroesComponent,
+  FlyingHeroesImpureComponent
+} from './flying-heroes.component';
 // import { HeroAsyncMessageComponent } from './hero-async-message.component';
 import { HeroBirthdayComponent } from './hero-birthday1.component';
 import { HeroBirthday2Component } from './hero-birthday2.component';
 // import { HeroListComponent } from './hero-list.component';
 import { PowerBoosterComponent } from './power-booster.component';
 import { PowerBoostCalculatorComponent } from './power-boost-calculator.component';
-// import {
-//   FlyingHeroesPipe,
-//   FlyingHeroesImpurePipe
-// } from './flying-heroes.pipe';
+import {
+  FlyingHeroesPipe,
+  FlyingHeroesImpurePipe
+} from './flying-heroes.pipe';
 // import { FetchJsonPipe } from './fetch-json.pipe';
 import { ExponentialStrengthPipe } from './exponential-strength.pipe';
 
@@ -29,16 +29,16 @@ import { ExponentialStrengthPipe } from './exponential-strength.pipe';
   ],
   declarations: [
     AppComponent,
-    // FlyingHeroesComponent,
-    // FlyingHeroesImpureComponent,
+    FlyingHeroesComponent,
+    FlyingHeroesImpureComponent,
     // HeroAsyncMessageComponent,
     HeroBirthdayComponent,
     HeroBirthday2Component,
     // HeroListComponent,
     PowerBoosterComponent,
     PowerBoostCalculatorComponent,
-    // FlyingHeroesPipe,
-    // FlyingHeroesImpurePipe,
+    FlyingHeroesPipe,
+    FlyingHeroesImpurePipe,
     // FetchJsonPipe,
     ExponentialStrengthPipe
   ],
