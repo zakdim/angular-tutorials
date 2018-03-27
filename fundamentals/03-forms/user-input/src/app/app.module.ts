7319781c0f919a8c1c7dfefe9bbe607992589763
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2018-03-27 09:03:12 -0400

    user-input

--- fundamentals/03-forms/user-input/src/app/app.module.ts
+++ fundamentals/03-forms/user-input/src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClickMeComponent } from './click-me.component';
+// import { ClickMe2Component } from './click-me2.component';
+// import {
+//   KeyUpComponent_v1,
+//   KeyUpComponent_v2,
+//   KeyUpComponent_v3,
+//   KeyUpComponent_v4
+// } from './keyup.components';
+// import { LittleTourComponent } from './little-tour.component';
+// import { LoopbackComponent } from './loop-back.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ClickMeComponent,
+    // ClickMe2Component,
+    // KeyUpComponent_v1,
+    // KeyUpComponent_v2,
+    // KeyUpComponent_v3,
+    // KeyUpComponent_v4,
+    // LittleTourComponent,
+    // LoopbackComponent
+  ],
+  providers: [
+
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
