4ba0340151e5ac75c89d9f4dccb3c1fd5986ffbb
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2018-03-26 23:41:25 -0400

    template drive forms in process

--- fundamentals/03-forms/forms/src/app/app.module.ts
+++ fundamentals/03-forms/forms/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
