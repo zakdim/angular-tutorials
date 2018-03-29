7e7add562f1322e09e4f19af24c01dfcc5be884d
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2018-03-29 17:08:08 -0400

    reactive forms init

--- fundamentals/03-forms/reactive-forms/src/app/app.module.ts
+++ fundamentals/03-forms/reactive-forms/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule
